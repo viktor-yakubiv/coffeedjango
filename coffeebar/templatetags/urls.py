73d821f5ae8cb84af611ef8ba96568099e219310
Viktor Yakubiv <viktor.yakubiv@gmail.com>
2016-12-10 15:31:15 +0200

    Fix i18n bugs

--- coffeebar/templatetags/urls.py
+++ coffeebar/templatetags/urls.py
@@ -1,15 +1,15 @@
 from django import template
-from django.core.urlresolvers import reverse
+from django.core.urlresolvers import reverse, resolve
 
 register = template.Library()
 
 
-@register.simple_tag
-def active(request, name, by_path=False):
+@register.simple_tag(takes_context=True)
+def active(context, name, by_path=False):
     """ Return the string 'active' current request.path is same as name
 
     Keyword aruguments:
-    request  -- Django request object
+    context  -- Django context object
     name     -- name of the url or the actual path
     by_path  -- True if name contains a url instead of url name
     """
@@ -18,7 +18,7 @@ def active(request, name, by_path=False):
     else:
         path = reverse(name)
 
-    if request.path == path:
+    if context['request'].path == path:
         return ' active '
 
     return ''
